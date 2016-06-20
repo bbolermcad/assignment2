2d24aa1182af860290b6b8c6dd556a56efab86bc
Brigit Boler <bboler@mcad.edu>
2016-06-19 21:43:48 -0500

    copying stuff works

--- sketch1/app.js
+++ sketch1/app.js
@@ -1,17 +1,19 @@
 function setup() {
+
   createCanvas(720, 360);
-  background(105);
-  noStroke()
+  background(222);
+  noStroke();
 
-  var gridSize=35;
+  var gridSize = 35;
 
-  for (var x = gridSize x <= width - gridSize; x += gridSize) {
+  for (var x = gridSize; x <= width - gridSize; x += gridSize) {
     for (var y = gridSize; y <= height - gridSize; y += gridSize) {
-    noStroke();
-    fill(255)
-    rect(x-1, y-1, 3, 3)
-    stroke(255, 100);
-    line(x, y, width/2, height/2)
+      noStroke();
+      fill(0);
+      rect(x-1, y-1, 3, 3);
+      stroke(0, 50);
+      line(x, y, width/2, height/2);
     }
   }
+
 }
--- sketch2/app.js
+++ sketch2/app.js
@@ -1,14 +1,38 @@
-  size(640, 360);
-  background(700);
+var y;
+var num = 14;
 
-  int gridSize=40;
+function setup() {
 
-  for (int x = gridSize x <= width - gridSize; x += gridSize) {
-    for (int y = gridSize; y <= height - gridSize; y += gridSize) {
-    noStroke();
-    fill(255)
-    rect(x-1, y-1, 3, 3)
-    stroke(255, 100);
-    line(x, y, width/2, height/2)
-    }
+  createCanvas(720, 360);
+  background(102);
+  noStroke();
+
+  // Draw white bars
+  fill(255);
+  y = 60;
+  for(var i = 0; i < num/3; i++) {
+    rect(50, y, 475, 10);
+    y+=20;
   }
+
+  // Gray bars
+  fill(51);
+  y = 40;
+  for(var i = 0; i < num; i++) {
+    rect(405, y, 30, 10);
+    y += 20;
+  }
+  y = 50;
+  for(var i = 0; i < num; i++) {
+    rect(425, y, 30, 10);
+    y += 20;
+  }
+
+  // Thin lines
+  y = 45;
+  fill(0);
+  for(var i = 0; i < num-1; i++) {
+    rect(120, y, 40, 1);
+    y+= 20;
+  }
+}
